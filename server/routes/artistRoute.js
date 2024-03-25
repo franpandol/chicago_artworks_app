9de884532d36c88f5be74adaf0b157c733f19c9f
FranPandol <pandol.francisco@gmail.com>
2024-03-25 11:05:35 -0300

    New endpoint to get detail from an artist

--- server/routes/artistRoute.js
+++ server/routes/artistRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {fetchArtworks} = require('../services/artworks_service');
 const { getFirestore } = require('firebase-admin/firestore');
 const db = getFirestore();
 
@@ -15,6 +16,25 @@ router.get('/', async (req, res, next) => {
   } catch (error) {
     console.error("Error fetching artists:", error);
     res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const doc = await db.collection('artists').doc(id).get();
+    if (!doc.exists) {
+      return res.status(404).json({ error: 'Artist not found.' });
+    }
+    const artist = doc.data();
+    await fetchArtworks().then(artworks => {
+      artist.artworks = artworks;
+    });
+    res.json(artist);
+  } catch (error) {
+    console.error("Error fetching artist details:", error);
+    res.status(500).json({ error: 'An error occurred while fetching artist details.' });
   }
 });
 
