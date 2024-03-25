dd80876cb34217173999cf7855bde3989e4945a3
FranPandol <pandol.francisco@gmail.com>
2024-03-25 18:50:25 -0300

    Init sentry only for production

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,33 +6,34 @@ import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType }
 
 import * as Sentry from "@sentry/react";
 
-Sentry.init({
-  dsn: `${import.meta.env.VITE_SENTRY_DSN}`,
+if (import.meta.env.PROD) {
+  Sentry.init({
+    dsn: `${import.meta.env.VITE_SENTRY_DSN}`,
 
-  integrations: [
-    // See docs for support of different versions of variation of react router
-    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect: React.useEffect,
-      useLocation,
-      useNavigationType,
-      createRoutesFromChildren,
-      matchRoutes
-    }),
-    Sentry.replayIntegration()
-  ],
+    integrations: [
+      Sentry.reactRouterV6BrowserTracingIntegration({
+        useEffect: React.useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes
+      }),
+      Sentry.replayIntegration()
+    ],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  tracesSampleRate: 1.0,
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    tracesSampleRate: 1.0,
 
-  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/chicago-service-o4iprcumoa-ue\.a\.run\.app/],
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+    // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: [`${import.meta.env.BASE_URL}`, ],
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
