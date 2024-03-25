7d8742e54ea18ed9ce15805f5b3f8bcd52145eeb
FranPandol <pandol.francisco@gmail.com>
2024-03-25 16:17:12 -0300

    Config public folder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../server/frontend/dist',
+    outDir: '../server/public',
   },
 })
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
+const writeLog = require('./logger');
+
+
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const compression = require("compression")
-
+const public_folder = 'public';
 const { initializeApp } = require('firebase-admin/app');
 initializeApp();
 
@@ -16,7 +19,7 @@ const PORT = process.env.PORT || 8000;
 // Enable All CORS Requests
 app.use(cors());
 app.use(express.json())
-app.use(express.static('frontend/dist'));
+app.use(express.static(public_folder));
 
 // Use morgan middleware for logging
 app.use(morgan('combined')); // 'combined' is one of the predefined formats in morgan
@@ -27,6 +30,8 @@ app.use(compression());
 // Centralized error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
+  writeLog(err.stack);
+
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
@@ -37,7 +42,7 @@ app.use('/api/artists', artistRoute);
 app.use('/api/artworks', artworksRoute);
 // Redirect all other requests to React app (if using React Router)
 app.get('*', (req, res) => {
-  res.sendFile('index.html', { root: 'frontend/dist' });
+  res.sendFile('index.html', { root: public_folder });
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
